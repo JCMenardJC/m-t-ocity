14409b5ca2dace2865b2263086347c70febb3b7f
MENARD Jean Christophe (SNCF VOYAGEURS / ETAB REGIONAL COMMERCIAL NOUVELLE AQUITAINE / TER ESC GARE BDX DTL NA) <8207636u@commun.ad.sncf.fr>
2022-11-24 00:00:30 +0100

    commit meteo city 0.2

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ const tMax = document.getElementById('max')
 const tMin = document.getElementById('min')
 const vent = document.getElementById('vent')
 const humidite = document.getElementById('humidite')
+const etatDuCiel = document.getElementById('ciel')
 
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     let textinput = input.value;
-    return coordonees(textinput)
+    return weather(textinput)
 });
 
-function coordonees(ville) {
+
+function weather(ville) {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville},FRS&units=metric&appid=8ac3c3fb7a8376c6b0bf6e83078f35bf`)
         .then((response) => response.json())
@@ -26,7 +28,86 @@ function coordonees(ville) {
             ressentie.innerHTML = 'Température ressentie : ' + data.main.feels_like + '°';
             tMax.innerHTML = 'Température maximale : ' + data.main.temp_max + '°';
             tMin.innerHTML = 'Température minimale : ' + data.main.temp_min + '°';
+            humidite.innerHTML = "Taux d'humidité : " + data.main.humidity + '%'
 
+            const direction = data.wind.deg
+
+            switch (direction) {
+
+                case 0:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Nord';
+                    break;
+
+                case 0 < direction < 45:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Nord/Nord/Est';
+                    break;
+
+                case 45:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Nord/Est';
+                    break;
+
+                case 45 < direction < 90:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Est/Nord/Est';
+                    break;
+
+                case 90:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Est';
+                    break;
+
+                case 90 < direction < 135:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Est/Sud/Est';
+                    break;
+
+                case 135:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Sud/Est';
+                    break;
+
+                case 135 < direction < 180:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Sud/Sud/Est';
+                    break;
+
+                case 180:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Sud';
+                    break;
+
+                case 180 < direction < 225:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Sud/Sud/Ouest';
+                    break;
+
+                case 225:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Sud/Ouest';
+                    break;
+
+                case (data.wind.deg > 225 && data.wind.deg < 270):
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Ouest/Sud/Ouest';
+                    break;
+
+                case 270:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Ouest';
+                    break;
+
+                case 270 < direction < 315:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Ouest/Nord/Ouest';
+                    break;
+
+                case 315:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Nord/Ouest';
+                    break;
+
+                case 315 < direction < 360:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Nord/Nord/Ouest';
+                    break;
+
+                default:
+                    vent.innerHTML = 'Vent : ' + data.wind.speed + 'Km/h' + ' Direction inconnue';
+
+            }
+            const ciel = data.weather[0].main
+            const img = document.getElementById('img')
+            if (ciel === 'Clouds') {
+                etatDuCiel.innerHTML = 'Nuageux';
+                img.src = "/images/cloudy-in-the-sky-landscape-wallpaper.jpg"
+            }
         }
 
         );
